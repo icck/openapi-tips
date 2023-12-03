d1d6f103011ecf2e7b8dc6728b2df918a62c830a
icck <icck.n.n1111@gmail.com>
2023-12-03 09:36:20 +0900

    chat history

--- app/app/st.py
+++ app/app/st.py
@@ -2,11 +2,25 @@ import streamlit as st
 
 st.title("Hello World")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
 prompt = st.chat_input("what is up?")
 
 if prompt:
+    # add message to session state
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
         response = "hello!!"
         st.markdown(response)
+
+    # add response to session state
+    st.session_state.messages.append({"role": "assistant", "content": response})
