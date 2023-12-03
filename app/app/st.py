7645d303e627c62355951d95f781c852856e1182
icck <icck.n.n1111@gmail.com>
2023-12-03 10:25:25 +0900

    chat open ai

--- app/app/st.py
+++ app/app/st.py
@@ -1,6 +1,11 @@
-import streamlit as st
+import os
 
-st.title("Hello World")
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+load_dotenv()
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -9,6 +14,8 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+
+st.title("Hello World")
 
 prompt = st.chat_input("what is up?")
 
@@ -19,8 +26,15 @@ if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        response = "hello!!"
-        st.markdown(response)
+        chat = ChatOpenAI(
+            model_name=os.environ.get("OPENAI_API_MODEL"),
+            api_key=os.environ.get("OPENAI_API_KEY"),
+            temperature=os.environ.get("OPENAPI_API_TEMPERATURE"),
+        )
+        messages = [HumanMessage(content=prompt)]
+
+        response = chat(messages)
+        st.markdown(response.content)
 
     # add response to session state
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response.content})
